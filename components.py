dd1835e189c68074d28e003430d3824bc4dcce1c
codepracticeat7 <albertmichael1378@gmail.com>
2025-09-29 18:22:20 +0100

    important bugs fixed

--- components.py
+++ components.py
@@ -11,7 +11,7 @@ def opening_file():
         try:
             path_file = input("Enter the file path: ")
             csv_file = open(path_file, errors="ignore")
-            
+            print(f"csv file {csv_file} opened from { path_file} using file opener")
             return path_file,csv_file
         except FileNotFoundError:
             print("File not found — please enter a correct file path.")
@@ -19,54 +19,63 @@ def opening_file():
             print("Unexpected error:", str(e))
 
 def hostid_search(host_id,csv_file):
-    countable=[]
-    csv_reader = csv.reader(csv_file)
-    for row in csv_reader:
+    try:
+        csv_file.seek(0)
+        countable=[]
+        csv_reader = csv.reader(csv_file)
+        for row in csv_reader:
 
-        if host_id==row[0]:
-            countable.append(host_id)
-            print (f"{row[1]}")
-            print(f" Host Description:  {row[2]},")
-            print(f" Host Name:         {row[3]},")
-            print(f" Host Location:     {row[5]}, ")
-            print(f" host since:        {row[4]}" )
-    if len(countable)==0:
-        print("entered id is invalid please run again")
-#         f=input("enter continue other search")
-#         if f==continue_other_search:
-#             break
-#         else:
+            if host_id==row[0]:
+                countable.append(host_id)
+                print (f"{row[1]}")
+                print(f" Host Description:  {row[2]},")
+                print(f" Host Name:         {row[3]},")
+                print(f" Host Location:     {row[5]}, ")
+                print(f" host since:        {row[4]}" )
+        if len(countable)==0:
+            print("entered id is invalid please run again")
+    #         f=input("enter continue other search")
+    #         if f==continue_other_search:
+    #             break
+    #         else:
+    except Exception as e:
+        print({e})
     
 
-def location_search(csv_file):
-    host_location=input("enter host location")
+def location_search(host_location,csv_file):
+    print("resetting pointer and creating csv reader")
+    csv_file.seek(0)
     csv_reader = csv.reader(csv_file)
+    
     countable2=[]
     for row in csv_reader:
-        if host_location in row[5]:
+        if host_location in {row[5]}:
+            print(row)
             countable2.append(host_location)
             print(f":{row[3]},")
             print(f"     {row[13]}, {row[20]},{row[21]},{row[22]}")
     if len(countable2)==0:
            print ("entered location is not valid")
-def ptp_search(csv_file):
-    j=input("enter type of property to search for room type, accommodates, bathrooms_text,bedrooms, beds")
+def ptp_search(csv_file,ptp_type):
     countable3=[]
+    csv_file.seek(0)
     csv_reader = csv.reader(csv_file)
     for row in csv_reader:
-        if j in row[13]:
-            countable3.append(j)
+        if ptp_type in row[13]:
+            countable3.append(ptp_type)
+            print(f" Data showing  room type, accommodates, bathrooms_text, bedrooms and  beds for {ptp_type} properties")
             print(f"{row[14:18]}  ")
     if len(countable3)==0:
             print("enter correct type of property")
-def review_search(csv_file):
-    host_location=input("enter the location to search for review details there: ")
+def review_search(csv_file,R):
+    print(f" Data showing review score rating, review score accuracy and review score cleanlines for Location {R}")
     print("review score rating  review score accuracy  review score cleanliness")
+    csv_file.seek(0)
     countable4=[]
     csv_reader = csv.reader(csv_file)
     for row in csv_reader:
-        if host_location in row[5]:
-            countable4.append(host_location)
+        if R in row[5]:
+            countable4.append(R)
             print(f"{row[27]},                        {row[28]},                 {row[29]}")
     if len(countable4)==0:
         print("enter correct host location")            
--- query_app.py
+++ query_app.py
@@ -4,21 +4,29 @@
 # In[2]:
 
 import csv
-from components import hostid_search,location_search,ptp_search,review_search
-
-print("path read")
-def app(csv_file):
+import components
+from components import hostid_search,location_search,ptp_search,review_search,opening_file
+def app(path_file,csv_file):
     try:
-        
-        print("file is opened")
-        host_id=input("enter host id")
-        hostid_search(host_id,csv_file)
-        #input("enter location")
-        location_search(csv_file)
-        #input("enter property type")
-        ptp_search(csv_file)
-        #input("enter a location to see review details")
-        review_search(csv_file)
-    except:
-        print("enter correct details")
-        app(csv_file)
+        while   True:
+            
+            host_id=input("enter host id")
+            
+            if host_id:
+                hostid_search(host_id,csv_file)
+            #input("enter location")
+            host_location=input("enter host location")
+            if host_location:
+                location_search(host_location,csv_file)
+            else:
+                print(f"not a valid {host_location}")
+            #input("enter property type")
+            ptp_type=input("enter type of property to search for room type, accommodates, bathrooms_text,bedrooms, beds")
+            if ptp_type:
+                ptp_search(csv_file,ptp_type)
+            #input("enter a location to see review details")
+            R=input("enter the location to search for review details there: ")
+            if host_location:
+                review_search(csv_file,R)
+    except Exception as e:
+        print(f"{e}enter correct details")
